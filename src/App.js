90218fe0e6e2ba24bc749a86e9a65214bb3ff899
ashikmahmud <ashikmamud187@gmail.com>
2022-05-22 02:27:22 +0600

    yeah create a file and folder stuctures and orginaize projects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./Pages/Blogs/Blogs";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 
 function App() {
   return (
     <>
-      <h1>First React Application</h1>
+      <Routes>
+         {/*  Normal Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/blogs" element={<Blogs />} />
+
+          
+      </Routes>
+      
     </>
   );
 }
