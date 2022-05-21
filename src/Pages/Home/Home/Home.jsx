b94e208209e3c7687f1f3c5c93fc2c81d06ec826
ashikmahmud <ashikmamud187@gmail.com>
2022-05-22 02:31:41 +0600

    create all additional page and ready to go

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import GetInTouch from "../GetInTouch/GetInTouch";
 import Hero from "../Hero/Hero";
+import Reviews from "../Reviews/Reviews";
+import Statistic from "../Statistic/Statistic";
+import Teams from "../Teams/Teams";
+import Tools from "../Tools/Tools";
 
 const Home = () => {
   return (
     <section>
       <Hero />
+      <Statistic />
+      <Tools />
+      <Teams />
+      <Reviews />
+      <GetInTouch />
     </section>
   );
 };
---
+++ src/Pages/Home/OrderStep/OrderStep.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderStep = () => {
+  return <div>OrderStep</div>;
+};
+
+export default OrderStep;
