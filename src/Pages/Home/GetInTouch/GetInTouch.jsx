03d46e7ce04c7941081767baccf4a86e7445a124
ashikmahmud <ashikmamud187@gmail.com>
2022-05-22 04:55:23 +0600

    added slider of hero section

--- src/Pages/Home/GetInTouch/GetInTouch.jsx
+++ src/Pages/Home/GetInTouch/GetInTouch.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import Slider from "react-slick";
 
 const GetInTouch = () => {
-  return <div>GetInTouch</div>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <div className="container mx-auto">
+      {" "}
+      <Slider {...settings}>
+        <div>
+          <img
+            src="https://media.istockphoto.com/photos/many-tools-picture-id492201907?k=20&m=492201907&s=612x612&w=0&h=IQ1-Uwst23ASNY1fxjIuJ28P42NTKKLRj9BP7yD0D4w="
+            alt=""
+          />
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 };
 
 export default GetInTouch;
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import Slider from "react-slick";
 
 const Hero = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
   return (
     <section
       className="bg-base-200 body-font py-40"
@@ -8,11 +18,32 @@ const Hero = () => {
     >
       <div className="hero  pb-40 bg-base-200">
         <div className="hero-content flex-col justify-between lg:flex-row-reverse">
-          <img
-            src="https://media.istockphoto.com/photos/many-tools-picture-id492201907?k=20&m=492201907&s=612x612&w=0&h=IQ1-Uwst23ASNY1fxjIuJ28P42NTKKLRj9BP7yD0D4w="
-            className="max-w-lg rounded-lg shadow-2xl"
-            alt=""
-          />
+          <div className="md:w-2/5 rounded shadow-2xl overflow-hidden cursor-grab">
+            <Slider {...settings}>
+              <div className="outline-none">
+                <img
+                  src="https://media.istockphoto.com/photos/many-tools-picture-id492201907?k=20&m=492201907&s=612x612&w=0&h=IQ1-Uwst23ASNY1fxjIuJ28P42NTKKLRj9BP7yD0D4w="
+                  alt=""
+                  className="max-w-lg rounded-lg "
+                />
+              </div>
+              <div className="outline-none">
+                <img
+                  src="https://media.istockphoto.com/photos/many-tools-picture-id492201907?k=20&m=492201907&s=612x612&w=0&h=IQ1-Uwst23ASNY1fxjIuJ28P42NTKKLRj9BP7yD0D4w="
+                  className="max-w-lg rounded-lg "
+                  alt=""
+                />
+              </div>
+              <div className="outline-none">
+                <img
+                  src="https://media.istockphoto.com/photos/many-tools-picture-id492201907?k=20&m=492201907&s=612x612&w=0&h=IQ1-Uwst23ASNY1fxjIuJ28P42NTKKLRj9BP7yD0D4w="
+                  className="max-w-lg rounded-lg "
+                  alt=""
+                />
+              </div>
+            </Slider>
+          </div>
+
           <div className="lg:w-2/4">
             <span className="text-lg">
               We are <strong>Tools House</strong>.
