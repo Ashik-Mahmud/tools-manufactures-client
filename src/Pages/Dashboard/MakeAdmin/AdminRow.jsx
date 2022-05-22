4e0084f39e7cee375b406620ba65278e46490a88
ashikmahmud <ashikmamud187@gmail.com>
2022-05-23 00:51:49 +0600

    done all the design of Dashboard

---
+++ src/Pages/Dashboard/MakeAdmin/AdminRow.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AdminRow = () => {
+  return (
+    <tr>
+      <th>1</th>
+      <td>45asdfasdfasdf</td>
+      <td>Ashik@gmail.com</td>
+      <td>
+        <button className="btn btn-success btn-sm">User</button>
+      </td>
+      <td>
+        <button className="btn btn-primary btn-sm">Make Admin</button>
+      </td>
+    </tr>
+  );
+};
+
+export default AdminRow;
--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.jsx
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import AdminRow from "./AdminRow";
 
 const MakeAdmin = () => {
-  return <div>MakeAdmin</div>;
+  return (
+    <div className="p-4">
+      <div className="title py-3">
+        <h3 className="text-2xl font-semibold">Sign Up Users</h3>
+        <span>all the user can see here</span>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>User Id</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <AdminRow />
+            <AdminRow />
+            <AdminRow />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MakeAdmin;
