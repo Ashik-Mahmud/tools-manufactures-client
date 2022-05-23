7df4caec43a9de9f975f30877cadad6eb9a2a202
ashikmahmud <ashikmamud187@gmail.com>
2022-05-24 05:25:28 +0600

    load order data into payment route

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,39 +1,64 @@
 import React from "react";
 import { MdArrowBackIos } from "react-icons/md";
+import { useQuery } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
+import Loader from "../../../Components/Loader/Loader";
+import auth from "../../../Firebase/Firebase.config";
 const Payment = () => {
   const { paymentId } = useParams();
   const navigate = useNavigate();
-  console.log(paymentId);
+  const { data, isLoading } = useQuery("Orders", () =>
+    fetch(`http://localhost:5000/orders?uid=${auth?.currentUser?.uid}`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) return <Loader />;
+
+  const singleOrder = data.result.find((order) => order._id === paymentId);
+
   return (
     <div className="grid place-items-center min-h-[80vh] px-3">
-      <div className="card shadow p-3 rounded w-full md:w-1/3">
-        <div className="card-body ">
+      <div className="card shadow  rounded w-full md:w-1/3">
+        <div className="card-body p-4">
           <div className="flex items-center gap-3">
             <MdArrowBackIos
               className="cursor-pointer"
               onClick={() => navigate(-1)}
             />
-            <h3 className="text-xl font-semibold">Product Name Goes here</h3>
+            <h3 className="text-xl font-semibold">
+              {singleOrder?.productInfo?.productName}
+            </h3>
           </div>
           <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8yM8W73QZ_kfxfNUZbmcWKIISTgMFLk7Tx7IzV_LDdfO_gT0kSukrDQL5h_K4MVCD6VI&usqp=CAU"
-            alt="stiper"
-            className="border rounded w-full my-3"
+            src={singleOrder?.productInfo?.image}
+            alt={singleOrder?.productInfo?.productName}
+            className="border rounded w-full h-52 object-contain my-3"
           />
-          <ul className="flex flex-wrap gap-2 items-center my-4">
-            <li>
-              Order Quantity - <strong>232pcs</strong>
+          <ul className="flex flex-wrap flex-col gap-2 items-start  my-4">
+            <li className="flex items-center justify-between w-full">
+              Order Quantity -{" "}
+              <strong>{singleOrder?.productInfo?.orderQty}pcs</strong>
             </li>
-            <li>
-              Name - <strong>Ashik Mahmud</strong>
+            <li className="flex items-center justify-between w-full">
+              Price
+              <strong>{singleOrder?.productInfo?.price}$</strong>
             </li>
-            <li>
-              Email - <strong>ashik@gmail.com</strong>
+            <li className="flex items-center justify-between w-full">
+              Name - <strong>{singleOrder?.author?.name}</strong>
+            </li>
+            <li className="flex items-center justify-between w-full">
+              Email - <strong>{singleOrder?.author?.email}</strong>
             </li>
           </ul>
           <div className="payment-option">
-            <button className="btn btn-primary">Pay $3435</button>
+            <button className="btn btn-primary">
+              Pay $
+              {Number(singleOrder?.productInfo?.orderQty) *
+                Number(singleOrder?.productInfo?.price)}
+            </button>
           </div>
         </div>
       </div>
