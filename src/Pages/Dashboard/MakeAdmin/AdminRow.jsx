e4618545d20365a93e4822acbccefb29270d6fc3
ashikmahmud <ashikmamud187@gmail.com>
2022-05-23 07:46:14 +0600

    create another api call for making admin

--- src/Pages/Dashboard/MakeAdmin/AdminRow.jsx
+++ src/Pages/Dashboard/MakeAdmin/AdminRow.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 
-const AdminRow = () => {
+const AdminRow = ({ uid, serialize, email }) => {
+  /* Handle Make Admin  */
+  const handleMakeAdmin = async (id) => {
+    await fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ role: "admin" }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
   return (
     <tr>
-      <th>1</th>
-      <td>45asdfasdfasdf</td>
-      <td>Ashik@gmail.com</td>
+      <th>{serialize + 1}</th>
+      <td>{uid}</td>
+      <td>{email}</td>
       <td>
-        <button className="btn btn-success btn-sm">User</button>
+        <button className="badge badge-success">User</button>
       </td>
       <td>
-        <button className="btn btn-primary btn-sm">Make Admin</button>
+        <button
+          onClick={() => handleMakeAdmin(uid)}
+          className="btn btn-primary btn-sm"
+        >
+          Make Admin
+        </button>
       </td>
     </tr>
   );
--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.jsx
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loader from "../../../Components/Loader/Loader";
+import auth from "../../../Firebase/Firebase.config";
 import AdminRow from "./AdminRow";
-
 const MakeAdmin = () => {
+  const { data, isLoading } = useQuery("Users", () =>
+    fetch(`http://localhost:5000/users/all?uid=${auth?.currentUser.uid}`, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  const usersData = data?.result;
   return (
     <div className="p-4">
       <div className="title py-3">
@@ -10,22 +21,26 @@ const MakeAdmin = () => {
       </div>
 
       <div className="overflow-x-auto">
-        <table className="table w-full">
-          <thead>
-            <tr>
-              <th></th>
-              <th>User Id</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <AdminRow />
-            <AdminRow />
-            <AdminRow />
-          </tbody>
-        </table>
+        {!isLoading ? (
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>User Id</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersData.map((user, ind) => (
+                <AdminRow key={user._id} {...user} serialize={ind} />
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <Loader />
+        )}
       </div>
     </div>
   );
