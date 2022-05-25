1414c80801a9034f22a7d0e56237d9758738641d
ashikmahmud <ashikmamud187@gmail.com>
2022-05-26 01:13:41 +0600

    create show all the single blog into the blog page

--- src/Pages/Blogs/BlogDetails.jsx
+++ src/Pages/Blogs/BlogDetails.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { BiSend } from "react-icons/bi";
 import { MdArrowBackIos } from "react-icons/md";
 import { useNavigate, useParams } from "react-router-dom";
+import Loader from "../../Components/Loader/Loader";
+import useBlog from "../../Hooks/useBlog";
 import Comment from "./Comment";
 
 const BlogDetails = () => {
   const navigate = useNavigate();
   const { blogId } = useParams();
+  const [blogs, loading] = useBlog();
+  if (!loading) return <Loader />;
+
+  const singleBlog = blogs.find((blog) => blog._id === blogId);
+
   return (
     <div className="blog-details py-10">
       <div className="container mx-auto shadow  rounded">
@@ -17,60 +24,24 @@ const BlogDetails = () => {
                 onClick={() => navigate(-1)}
                 className="cursor-pointer"
               />
-              <h2 className="text-2xl font-semibold">Blogs Title goes here</h2>
+              <h2 className="text-2xl font-semibold">{singleBlog?.title}</h2>
             </div>
             <div className="image h-52 w-full rounded-lg bg-base-300 grid place-items-center">
               Image Not Loaded
             </div>
             <ul className="meta flex items-center gap-4 my-2 flex-wrap">
               <li>
-                Category - <strong>Tech</strong>
+                Category - <strong>{singleBlog?.category}</strong>
               </li>
               <li>
-                CreateAt - <strong>23 Jan, 2022</strong>
+                CreateAt - <strong>{singleBlog?.createAt}</strong>
               </li>
               <li>
-                Author - <strong>Ashik Mahmud</strong>
+                Author - <strong>{singleBlog?.author?.name}</strong>
               </li>
             </ul>
-            <p className="desc text-slate-500 leading-7 my-5 text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-              dolorem maxime dolore? Soluta quam distinctio, quaerat itaque
-              deserunt facilis eveniet tempora pariatur recusandae quasi
-              reiciendis earum obcaecati quis, officiis provident laudantium
-              commodi tenetur reprehenderit illo inventore, necessitatibus
-              repellat molestias fugit non. Doloribus sint quibusdam, modi
-              nostrum ipsum ex atque repudiandae, explicabo esse sunt eos.
-              Eligendi unde rerum veniam sapiente pariatur vitae ipsum
-              repudiandae quisquam! Itaque, nam incidunt adipisci quisquam
-              repudiandae culpa laudantium ab minima non architecto laboriosam
-              veniam aliquid eius voluptatum qui fuga minus aut! Quod aspernatur
-              similique dolorem facere? Eligendi voluptates ipsam ipsa tempora
-              excepturi minus vero quae fuga soluta possimus animi, atque
-              molestias alias explicabo deserunt. Provident perferendis facilis
-              a eius dolore possimus, iure autem doloremque voluptatum dolorem,
-              expedita numquam atque sed eveniet quos aperiam blanditiis harum
-              doloribus eaque, eligendi minima voluptate itaque porro?
-              Architecto, velit. Nam quos repudiandae laborum aliquid harum sunt
-              quisquam atque ducimus perspiciatis doloribus esse, vero
-              accusantium ipsam itaque hic consequatur asperiores accusamus
-              necessitatibus saepe vel eligendi officia? Quibusdam rem neque
-              itaque molestiae, cumque sequi unde molestias nesciunt, temporibus
-              ut voluptatibus illum maxime ducimus tenetur at perspiciatis
-              aliquam minima, quas libero. Doloremque rem illo est suscipit
-              molestias aut dicta. Perspiciatis cum pariatur exercitationem,
-              provident, a ab sapiente sunt aut magni in voluptatibus, nemo
-              repudiandae ad! Minus, similique quidem commodi consectetur quam
-              voluptatum quos dolores doloremque itaque soluta facere eaque
-              voluptas ipsum magni labore amet dicta veritatis cumque ab! Nulla
-              magnam, inventore amet culpa totam facere corporis, quibusdam, et
-              cupiditate rerum consequuntur. Culpa ex corporis facere recusandae
-              in, distinctio nobis laboriosam rerum tenetur et magnam iste ipsum
-              nemo consequatur minus tempora non cum optio doloribus.
-              Repellendus velit nam porro deleniti dolor, facilis obcaecati? Est
-              veritatis dolores, nemo illum unde, aut tempora nam at, eos dolore
-              delectus recusandae natus blanditiis autem! Earum ab consectetur
-              enim rem.
+            <p className="desc text-slate-500 leading-7 my-5 text-md">
+              {singleBlog?.description}
             </p>
           </div>
           <div className="card-footer">
--- src/Pages/Blogs/Comment.jsx
+++ src/Pages/Blogs/Comment.jsx
@@ -4,14 +4,14 @@ import { BiTrash } from "react-icons/bi";
 const Comment = () => {
   return (
     <div className="comment flex gap-8 bg-base-200 p-2 rounded-md">
-      <div className="avatar w-16 border rounded overflow-hidden">
+      <div className="avatar w-28 lg:w-20 border rounded overflow-hidden">
         <img
           src="https://lh3.googleusercontent.com/a-/AOh14GgJ3jEJ2pgW5pURY-Pzi3ayUjZPrbE82cPa8BBEZw=s96-c"
           alt="avatar"
         />
       </div>
       <div className="details w-11/12">
-        <div className="info flex items-center justify-between">
+        <div className="info flex items-center justify-between flex-wrap">
           <h3 className="text-md font-semibold">Ashik Mahmud</h3>
           <div className="date">23 Jan, 2022 at 6:45PM</div>
         </div>
